9005dc339768bdca90b2e6ffde3e6651f7d46188
Dorita Nicole Thomas <95882149+nicole092003@users.noreply.github.com>
2022-04-13 20:35:09 -0400

    revision

---
+++ bmi_revised.js
@@ -0,0 +1,13 @@
+document.getElementById("CalcBMI").addEventListener('click.button',doCalcBMI);
+
+function doCalcBMI(){
+    var theWeight = document.getElementById("myPounds").value;
+    var theHeight = document.getElementById("myHeight").value;
+    var myBMI = (theWeight / (theWeight * Math.pow(2)(theHeight*703)
+    (myBMI)  (myBMI.toFixed(2))
+    (document.getElementById("bmiResult").innerHTML = myOutput(myBMI)
+}
+function myOutput(getBMI){
+    let theString = "Your body mass index (BMI) is: "  
+    return theString;
+}
